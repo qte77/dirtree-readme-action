a5440e3ffc392cfeef85c32d948e44f2258db364
qte77 <93844790+qte77@users.noreply.github.com>
2024-06-08 20:52:47 +0200

    Changed out csv newline to None, Added \n in out strings

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ if EXCLUDE is not None:
 if not os.path.exists(OUT_FILE):
   # folder needs to exist before open() context
   os.makedirs(os.path.dirname(OUT_FILE), exist_ok=True)
-  out.append([str(datetime.now(timezone.utc))])
+  out.append([f"{datetime.now(timezone.utc)}\n"])
 
 print(os.walk('.'))
 
@@ -20,15 +20,16 @@ for dirname, dirnames, filenames in os.walk('.'):
   indent = " " * (k+1)
   indent_f = " " * (k+2)
   for ex in EXCLUDE:
-    if ex in dirnames: dirnames.remove(ex)
+    if ex in dirnames:
+      dirnames.remove(ex)
   for subdirname in dirnames:
     dir = os.path.join(dirname, subdirname)
-    out.append([f"{indent}{dir}"])
+    out.append([f"{indent}{dir}\n"])
   for filename in filenames:
     file = os.path.join(dirname, filename)
-    out.append([f"{indent_f}{file}"])
+    out.append([f"{indent_f}{file}\n"])
 
-with open(OUT_FILE, 'a+', newline='', encoding='UTF8') as f:
+with open(OUT_FILE, 'a+', newline=None, encoding='UTF8') as f:
   writer = csv.writer(f)
   for o in out:
     writer.writerow(o)
