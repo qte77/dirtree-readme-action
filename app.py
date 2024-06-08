895d7247acebbed63deab7c014ba89a1793d9130
qte77 <93844790+qte77@users.noreply.github.com>
2024-06-08 21:08:19 +0200

    Removed array for out list strings for open() f.write()

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ if EXCLUDE is not None:
 if not os.path.exists(OUT_FILE):
   # folder needs to exist before open() context
   os.makedirs(os.path.dirname(OUT_FILE), exist_ok=True)
-  out.append([f"{datetime.now(timezone.utc)}"])
+  out.append(str(datetime.now(timezone.utc)))
 
 for dirname, dirnames, filenames in os.walk('.'):
   indent = " " * (k+1)
@@ -24,10 +24,10 @@ for dirname, dirnames, filenames in os.walk('.'):
       dirnames.remove(ex)
   for subdirname in dirnames:
     dir = os.path.join(dirname, subdirname)
-    out.append([f"{indent}{dir}"])
+    out.append(f"{indent}{dir}")
   for filename in filenames:
     file = os.path.join(dirname, filename)
-    out.append([f"{indent_f}{file}"])
+    out.append(f"{indent_f}{file}")
 
 with open(OUT_FILE, 'a+', newline=None, encoding='UTF8') as f:
   f.write("```sh")
