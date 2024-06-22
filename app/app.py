03468f59465384b657ef0bde1ce92f20e1eb8952
qte77 <93844790+qte77@users.noreply.github.com>
2024-06-23 00:27:39 +0200

    Refactored app.py

--- app/app.py
+++ app/app.py
@@ -16,11 +16,7 @@ branch = '│   '
 tee = '├── '
 last = '└── '
 
-if not exists(OUT_FILE):
-  # folder needs to exist before open() context
-  makedirs(dirname(OUT_FILE), exist_ok=True)
-
-out.append(f"\n{datetime.now(timezone.utc)}")
+assert exists(startpath), f"{startpath} not found. Aborting"
 
 def find_exclusion_overlap(root: str, exclude_list: list) -> bool:
   '''Return True if any of exclude in root split by "/", else False'''
@@ -48,11 +44,16 @@ def tree(dir_path: Path, prefix: str=''):
             extension = branch if pointer == tee else space 
             yield from tree(path, prefix=prefix+extension)
 
-for line in tree(startpath):
+if not exists(OUT_FILE):
+  # folder needs to exist before open() context
+  makedirs(dirname(OUT_FILE), exist_ok=True)
+
+out.append(f"\n```{CMD_HIGHLIGHT}")
+out.append(f"\n{datetime.now(timezone.utc)}")
+for line in tree(Path(startpath)):
     out.append(line)
+out.append("```\n")
 
 with open(OUT_FILE, 'a+', newline=None, encoding='UTF8') as f:
-  f.write(f"\n```{CMD_HIGHLIGHT}")
   for o in out: # reversed(out):
     f.write(f"{o}\n")
-  f.write("```\n")
