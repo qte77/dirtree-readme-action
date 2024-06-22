b79064b20ecfdcd1218a1cbe71740447bbf05248
qte77 <93844790+qte77@users.noreply.github.com>
2024-06-23 01:08:59 +0200

    Move functions to utils.py

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 from os import getenv
 from datetime import datetime, timezone
 from pathlib import Path
+import .utils
 
 OUT_FILE = str(getenv("OUT_FILE", 'data/dummy-data.md'))
 EXCLUDE = str(getenv("EXCLUDE", '.git')) # separated by |
@@ -16,43 +17,6 @@ tee = '├── '
 last = '└── '
 
 assert startpath.exists(), f"{startpath} not found. Aborting"
-
-def find_exclusion_overlap(path: Path, exclude_list: list) -> bool:
-  '''Return True if any of exclude in path split by "/", else False'''
-  assert isinstance(path, Path) and isinstance(exclude_list, list)
-  parts = path.parts
-  print(parts)
-  for ex in exclude_list:
-    if ex in parts:
-      return True
-  return False
-
-# https://stackoverflow.com/questions/9727673/list-directory-tree-structure-in-python/59109706
-def tree(path: Path, prefix: str=''):
-    """
-    A recursive generator, given a directory Path object
-    will yield a visual tree structure line by line
-    with each line prefixed by the same characters
-    """
-    if find_exclusion_overlap(path, exclude_list):
-      return None
-    contents = list(path.iterdir())
-    # contents each get pointers that are ├── with a final └── :
-    pointers = [tee] * (len(contents) - 1) + [last]
-    for pointer, path in zip(pointers, contents):
-        yield prefix + pointer + path.name
-        if path.is_dir():
-            extension = branch if pointer == tee else space 
-            yield from tree(path, prefix=prefix+extension)
-
-def get_tree(startpath: Path):
-  out = []
-  out.append(f"\n```{CMD_HIGHLIGHT}")
-  out.append(f"\n{datetime.now(timezone.utc)}")
-  for line in tree(startpath):
-      out.append(line)
-  out.append("```\n")
-  return out
 
 if not outfpath.parent.exists():
   # folder needs to exist before open() context
