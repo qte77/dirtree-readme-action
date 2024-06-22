44ef9488b9cc4a8d12ec2f20713a19a7ee5bdce8
qte77 <93844790+qte77@users.noreply.github.com>
2024-06-23 00:48:47 +0200

    Fixed variable root name

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from os import getenv, makedirs 
-from os.path import dirname, exists
+from os.path import dirname
 from datetime import datetime, timezone
 from pathlib import Path
 
@@ -8,7 +8,7 @@ EXCLUDE = str(getenv("EXCLUDE", '.git')) # separated by |
 CMD_HIGHLIGHT = str(getenv("CMD_HIGHLIGHT", 'sh'))
 
 out = []
-startpath = '.'
+startpath = Path('.')
 exclude_list = EXCLUDE.split('|')
 
 space = '    '
@@ -16,26 +16,26 @@ branch = '│   '
 tee = '├── '
 last = '└── '
 
-assert exists(startpath), f"{startpath} not found. Aborting"
+assert startpath.exists(), f"{startpath} not found. Aborting"
 
 def find_exclusion_overlap(path: Path, exclude_list: list) -> bool:
-  '''Return True if any of exclude in root split by "/", else False'''
+  '''Return True if any of exclude in path split by "/", else False'''
   assert isinstance(path, Path) and isinstance(exclude_list, list)
   for ex in exclude_list:
-    if ex in root.split('/'):
+    if ex in path.parts:
       return True
-  return False  
+  return False
 
 # https://stackoverflow.com/questions/9727673/list-directory-tree-structure-in-python/59109706
-def tree(dir_path: Path, prefix: str=''):
+def tree(path: Path, prefix: str=''):
     """
     A recursive generator, given a directory Path object
     will yield a visual tree structure line by line
     with each line prefixed by the same characters
     """    
-    if find_exclusion_overlap(dir_path, exclude_list):
+    if find_exclusion_overlap(path, exclude_list):
       pass
-    contents = list(dir_path.iterdir())
+    contents = list(path.iterdir())
     # contents each get pointers that are ├── with a final └── :
     pointers = [tee] * (len(contents) - 1) + [last]
     for pointer, path in zip(pointers, contents):
@@ -50,7 +50,7 @@ if not exists(OUT_FILE):
 
 out.append(f"\n```{CMD_HIGHLIGHT}")
 out.append(f"\n{datetime.now(timezone.utc)}")
-for line in tree(Path(startpath)):
+for line in tree(startpath):
     out.append(line)
 out.append("```\n")
 
