6f6fef8e42a3fcd4549569e22f3dbf8b70f644c3
qte77 <93844790+qte77@users.noreply.github.com>
2024-06-23 00:18:10 +0200

    Added recursive tree yield

--- app/app.py
+++ app/app.py
@@ -28,28 +28,26 @@ def find_exclusion_overlap(root: str, exclude_list: list) -> bool:
       return True
   return False  
 
-# https://stackoverflow.com/a/9728478/list-directory-tree-structure-in-python
-# https://stackoverflow.com/a/59109706/list-directory-tree-structure-in-python
-# def get_dir_content(startpath):
-#  dir = 
-#  return
+# https://stackoverflow.com/questions/9727673/list-directory-tree-structure-in-python/59109706
+def tree(dir_path: Path, prefix: str=''):
+    """
+    A recursive generator, given a directory Path object
+    will yield a visual tree structure line by line
+    with each line prefixed by the same characters
+    """    
+    if find_exclusion_overlap(dir_path, exclude_list):
+      pass
+    contents = list(dir_path.iterdir())
+    # contents each get pointers that are ├── with a final └── :
+    pointers = [tee] * (len(contents) - 1) + [last]
+    for pointer, path in zip(pointers, contents):
+        yield prefix + pointer + path.name
+        if path.is_dir():
+            extension = branch if pointer == tee else space 
+            yield from tree(path, prefix=prefix+extension)
 
-for root, dirs, files in os.walk(startpath):
-    if find_exclusion_overlap(root, exclude_list):
-      continue
-    level = root.replace(startpath, '').count(os.sep)
-    num_dirs = len(dirs) - 1 # root is first dir
-    num_files = len(files)
-    indent = ( branch if num_dirs > level else space ) * ( level - 1 )
-    indent_d = indent if level < 1 else indent + ( tee if num_dirs > 1 else last )
-    out.append(
-      f'{indent_d}{os.path.basename(root)}/'
-      f'# DEBUG::{level=},{num_dirs=},{num_files=}'
-    )
-    for i, f in enumerate(files):
-      indent_f = indent_d + ( "" if level < 1 else space ) + ( tee if (num_files-1 > i and num_dirs < 1) else last )
-      out.append(f'{indent_f}{f}')
-    
+for line in tree(startpath):
+    out.append(line)
 
 with open(OUT_FILE, 'a+', newline=None, encoding='UTF8') as f:
   f.write(f"\n```{CMD_HIGHLIGHT}")
