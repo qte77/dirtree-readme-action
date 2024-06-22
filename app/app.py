b77b9d0eb75bcd9313b94269ba7533cee022e8b4
qte77 <93844790+qte77@users.noreply.github.com>
2024-06-23 00:01:32 +0200

    Prepare for recursing listdir

--- app/app.py
+++ app/app.py
@@ -30,18 +30,26 @@ def find_exclusion_overlap(root: str, exclude_list: list) -> bool:
 
 # https://stackoverflow.com/a/9728478/list-directory-tree-structure-in-python
 # https://stackoverflow.com/a/59109706/list-directory-tree-structure-in-python
+# def get_dir_content(startpath):
+#  dir = 
+#  return
+
 for root, dirs, files in os.walk(startpath):
     if find_exclusion_overlap(root, exclude_list):
       continue
     level = root.replace(startpath, '').count(os.sep)
-    num_dirs = len(dirs)
+    num_dirs = len(dirs) - 1 # root is first dir
     num_files = len(files)
     indent = ( branch if num_dirs > level else space ) * ( level - 1 )
     indent_d = indent if level < 1 else indent + ( tee if num_dirs > 1 else last )
-    out.append(f'{indent_d}{os.path.basename(root)}/ # DEBUG::{level=},{num_dirs=},{num_files=}')
+    out.append(
+      f'{indent_d}{os.path.basename(root)}/'
+      f'# DEBUG::{level=},{num_dirs=},{num_files=}'
+    )
     for i, f in enumerate(files):
       indent_f = indent_d + ( "" if level < 1 else space ) + ( tee if (num_files-1 > i and num_dirs < 1) else last )
       out.append(f'{indent_f}{f}')
+    
 
 with open(OUT_FILE, 'a+', newline=None, encoding='UTF8') as f:
   f.write(f"\n```{CMD_HIGHLIGHT}")
