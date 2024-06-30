7505c92efb143bf701eabd43781c415568f80e6b
qte77 <93844790+qte77@users.noreply.github.com>
2024-07-01 00:09:05 +0200

    Changed to single open() and search for insert index

--- app/app.py
+++ app/app.py
@@ -17,14 +17,17 @@ if not outfpath.parent.exists():
   # folder needs to exist before open() context
   outfpath.parent.mkdir(parents=True, exist_ok=True)
 
-with open(outfpath, "r") as f:
-    contents = f.readlines()
+dirtree = get_tree_output(startpath, exclude_list, CMD_HIGHLIGHT)
 
-contents.insert(
-  INSERT_HERE_STRING,
-  get_tree_output(startpath, exclude_list, CMD_HIGHLIGHT)
-)
-
-with open(outfpath, 'a+', newline=None, encoding='UTF8') as f:
-  # contents = "".join(contents)
-  f.write(contents)
+with open(outfpath, 'r+') as f:
+    contents = fd.readlines()
+    if INSERT_HERE_STRING in contents[-1]:
+        contents.append(dirtree)
+    else:
+        for index, line in enumerate(contents):
+            # only insert afet first match
+            if INSERT_HERE_STRING in line and dirtree[0] not in contents[index + 1]:
+                contents.insert(index + 1, dirtree)
+                break
+    fd.seek(0)
+    fd.writelines(contents)
