529f9e1e89776bd6af89d91a80ead07c0654d66a
qte77 <93844790+qte77@users.noreply.github.com>
2024-07-01 00:01:27 +0200

    Added insert after INSERT_HERE_STRING

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ from os import getenv
 from pathlib import Path
 from utils import get_tree_output
 
-OUT_FILE = str(getenv("OUT_FILE", 'data/dummy-data.md'))
+OUT_FILE = str(getenv("OUT_FILE", 'data/dummy-readme.md'))
 EXCLUDE = str(getenv("EXCLUDE", '.git|__pycache__')) # string separated by |
 CMD_HIGHLIGHT = str(getenv("CMD_HIGHLIGHT", 'sh'))
 INSERT_HERE_STRING = str(getenv("INSERT_HERE_STRING", '<!-- DIRTREE-README-ACTION-INSERT-HERE -->'))
@@ -17,6 +17,14 @@ if not outfpath.parent.exists():
   # folder needs to exist before open() context
   outfpath.parent.mkdir(parents=True, exist_ok=True)
 
+with open(outfpath, "r") as f:
+    contents = f.readlines()
+
+contents.insert(
+  INSERT_HERE_STRING,
+  get_tree_output(startpath, exclude_list, CMD_HIGHLIGHT)
+)
+
 with open(outfpath, 'a+', newline=None, encoding='UTF8') as f:
-  for o in get_tree_output(startpath, exclude_list, CMD_HIGHLIGHT):
-    f.write(o)
+  # contents = "".join(contents)
+  f.write(contents)
