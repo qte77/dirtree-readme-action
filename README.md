1952adeb55a3bd32cf7335c2a68d035f822b0875
qte77 <93844790+qte77@users.noreply.github.com>
2024-07-01 17:18:23 +0200

    PR dev-create-themes (#18)
    * Added indicator themes
    
    * Added TREE_THEME to env var table
    
    * Added TREE_THEME
    
    * Added TREE_THEME
    
    * Added TREE_THEME
    
    * Updated README.md
    
    * Trying TREE_THEME sh
    
    * Updated README.md
    
    * Added write_to_file()
    
    * Separated write into utils.py::write_to_file()
    
    * Removed left over variables for write
    
    * Added missing variables to write_to_file()
    
    * Added decription to env var table
    
    * Added debug print for _get_tree_theme() return
    
    * Fixed print bracket typo
    
    * Added tuple expansion to get tree theme
    
    * Removed dirtree
    
    * Added tree_theme to path recursion
    
    * Removed dirtree check isinstance lsit before writing to file
    
    * Added print debug for write to fil
    
    * Format functions
    
    * Added default env vars
    
    * Separated default env vars into utils.py
    
    * Fixed typeo EXCLUDE
    
    * Moved defaults to app.py
    
    * Added param tree_theme type to get_tree_output()
    
    * Added default env vars
    
    * Moved get tree them to get_tree_output()
    
    * Added get_write_positions_in_file()
    
    * Refactored to insert get_write_positions_in_file()
    
    * Fixed missing line continuation
    
    * Fixed debug print
    
    * Added debug print
    
    * Added check isinstance()
    
    * Added index debug
    
    * Added print debug
    
    * Fixed getnv missing default
    
    * Removed get positions print debug
    
    * Updated README.md
    
    * Test ellis tree theme
    
    * Updated README.md
    
    * Test tree theme elli
    
    * Updated README.md
    
    * Test tree theme cmd
    
    * Updated README.md
    
    * Test tree theme null
    
    * Fixed ruff 9:3 F541 [*] f-string without any placeholders
    
    * Updated README.md
    
    * Test tree theme slash
    
    * Updated README.md
    
    * Updated README.md
    
    * Test tree theme default
    
    * Updated README.md
    
    * Test tree theme slash
    
    * Updated README.md
    
    * Changed theme parts
    
    * Updated README.md
    
    * Test tree theme elli
    
    * Updated README.md
    
    * Changed tree theme elli
    
    * Updated README.md
    
    * Changed tree theme ellis
    
    * Updated README.md
    
    * Switching to tree theme default
    
    * Updated README.md
    
    ---------
    
    Co-authored-by: GitHub <action@github.com>

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Copy directory tree into file, e.g. README.md, instead of manual effort.
 
 <!-- DIRTREE-README-ACTION-INSERT-HERE-START -->
 ```sh
-2024-07-01 02:27:11.968021+00:00
+2024-07-01 14:47:20.310741+00:00
 ├── app
 │   ├── utils.py
 │   └── app.py
@@ -35,7 +35,7 @@ Copy directory tree into file, e.g. README.md, instead of manual effort.
 | Name | Default |
 | - | - |
 | APP | 'app/app.py' |
-| CMD_HIGHLIGHT | 'sh' |
+| CMD_HIGHLIGHT | 'sh' # All Github supported for code blocks |
 | EXCLUDE | '.git\|\_\_pycache\_\_' # separated by \| |
 | GH_EMAIL | 'action@github.com' |
 | GH_NAME | 'GitHub Action' |
@@ -43,3 +43,4 @@ Copy directory tree into file, e.g. README.md, instead of manual effort.
 | INSERT_HERE_END_STRING | '<\!-- DIRTREE-README-ACTION-INSERT-HERE-END -->' |
 | OUT_FILE | 'data/dummy-readme.md' |
 | PY_VER | '3.10' |
+| TREE_THEME | 'sh' # ['cmd', 'slash', 'elli', 'null', 'sh'] |
--- app/utils.py
+++ app/utils.py
@@ -1,14 +1,30 @@
 '''Contains utility functions for Github dirtree-readme-action'''
 
+
 from datetime import datetime, timezone
 from pathlib import Path
 
-space = '    '
-branch = '│   '
-tee = '├── '
-last = '└── '
 
-def is_path_in_exclude(path: Path, exclude_list: list) -> bool:
+def _get_tree_theme(theme: str) -> tuple:
+  '''
+  Returns tuple of tree indicator themes: space, branch, tee, last.
+  Offers 'cmd', 'slash', 'elli', 'null', 'sh'. Defaults to 'sh'.
+  '''
+  if theme == 'cmd':
+    return '   ', '│  ', '├──', '└──'
+  if theme == 'slash':
+    return '    ', '│   ', '│── ', '\── '
+  if theme == 'elli':
+    return '    ', '︙   ', '︙··· ', ' ···· '
+  if theme == 'null':
+    return '    ', '    ', '    ', '    '
+  else: 
+    return '    ', '│   ', '├── ', '└── '
+
+
+def _is_path_in_exclude(
+  path: Path, exclude_list: list
+) -> bool:
   '''Return True if any of exclude_list in path, else False'''
   assert isinstance(path, Path)
   assert isinstance(exclude_list, list)
@@ -17,33 +33,89 @@ def is_path_in_exclude(path: Path, exclude_list: list) -> bool:
       return True
   return False
 
-# https://stackoverflow.com/questions/9727673/list-directory-tree-structure-in-python/59109706
-def generate_tree(path: Path, exclude_list: list, prefix: str = ''):
+
+# list-directory-tree-structure-in-python:
+# https://stackoverflow.com/a/59109706
+def _generate_tree(
+  path: Path, exclude_list: list,
+  space: str, branch: str, tee: str, last: str,
+  prefix: str = ''
+) -> str:
   '''
   A recursive generator, given a directory Path object
   will yield a visual tree structure line by line
-  with each line prefixed by the same characters
+  with each line prefixed by the same characters.
+  Returns a string of the current folder or file
+  and hierarchical indicators.
   '''
   contents = list(path.iterdir())
-  # contents each get pointers that are ├── with a final └── :
+  # contents each get pointers that are 'tee' with a final 'last'
   pointers = [tee] * (len(contents) - 1) + [last]
   for pointer, path in zip(pointers, contents):
-    if not is_path_in_exclude(path, exclude_list):
+    if not _is_path_in_exclude(path, exclude_list):
       yield prefix + pointer + path.name
       if path.is_dir():
           extension = branch if pointer == tee else space
-          yield from generate_tree(
-            path, exclude_list, prefix + extension
+          yield from _generate_tree(
+            path, exclude_list,
+            space, branch, tee, last,
+            prefix + extension
           )
 
+
 def get_tree_output(
-  startpath: Path, exclude_list: list, cmd_highlight: str
+  startpath: Path, exclude_list: list,
+  cmd_highlight: str, tree_theme: str
 ) -> list:
   '''Returns a list of startpath and its children'''
+  space, branch, tee, last = _get_tree_theme(tree_theme)
   out = []
   out.append(f"```{cmd_highlight}\n")
   out.append(f"{datetime.now(timezone.utc)}\n")
-  for line in generate_tree(startpath, exclude_list):
+  for line in _generate_tree(
+    startpath, exclude_list,
+    space, branch, tee, last
+  ):
       out.append(f"{line}\n")
   out.append("```\n")
   return out
+
+
+def get_write_positions_in_file(
+  outfpath: Path, start_string: str, end_string: str
+) -> tuple:
+  '''
+  Returns position of first consecutive start_string
+  and end_string.
+  '''
+  sdx, edx = None, None
+  with open(outfpath, 'r') as f_in:
+    for index, line in enumerate(f_in):
+      if line.startswith(start_string):
+        sdx = index
+      elif line.startswith(end_string) and sdx:
+        edx = index
+        break
+  return sdx, edx
+
+
+def write_to_file(
+  outfpath: Path, dirtree: list,
+  start_index: int, end_index: int
+) -> None:
+  '''Replaces content between indices start_index and end_index'''
+  outfpath_temp = outfpath.with_suffix(".temp_outfile_ghact")
+  printed = False
+  assert start_index >= 0 and end_index >= 1, \
+    f"Can not insert: {start_index=}, {end_index=}"
+  with open(outfpath, 'r') as f_in:
+    with open(outfpath_temp, 'w') as f_out:
+      for index, line in enumerate(f_in):
+        if index <= start_index or index >= end_index:
+          f_out.write(line)
+        elif not printed:
+          for o in dirtree:
+            f_out.write(o)
+          printed = True
+  outfpath.unlink() # missing_ok=True
+  outfpath_temp.rename(outfpath)
