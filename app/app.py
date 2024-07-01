da2fa8ba2ad943cde0464c712c1a388d8d2ba76c
qte77 <93844790+qte77@users.noreply.github.com>
2024-07-01 16:01:56 +0200

    Refactored to insert get_write_positions_in_file()

--- app/app.py
+++ app/app.py
@@ -1,6 +1,9 @@
 from os import getenv
 from pathlib import Path
-from utils import get_tree_output, write_to_file
+from utils import (
+  get_tree_output, write_to_file,
+  get_write_positions_in_file  
+)
 
 
 CMD_HIGHLIGHT = str(getenv("CMD_HIGHLIGHT", 'sh'))
@@ -23,16 +26,21 @@ outfpath = Path(OUT_FILE)
 startpath = Path('.')
 
 
+assert outfpath.exists(), f"{outfpath} not found. Aborting"
 assert startpath.exists(), f"{startpath} not found. Aborting"
-if not outfpath.parent.exists():
-  # folder needs to exist before open() context
-  outfpath.parent.mkdir(parents=True, exist_ok=True)
 
 
-dirtree = get_tree_output(
-  startpath, exclude_list, CMD_HIGHLIGHT, TREE_THEME
+start_index, end_index = get_write_positions_in_file(
+  outfpath, INSERT_HERE_START_STRING, INSERT_HERE_END_STRING
 )
-write_to_file(
-  outfpath, dirtree,
-  INSERT_HERE_START_STRING, INSERT_HERE_END_STRING
-)
+if start_index >= 0 and end_index >= 1:
+  dirtree = get_tree_output(
+    startpath, exclude_list, CMD_HIGHLIGHT, TREE_THEME
+  )
+  write_to_file(
+    outfpath, dirtree, start_index, end_index
+  )
+else:
+  print(
+    f"Could not print: {start_index=}, END {end_index=}"
+  )
