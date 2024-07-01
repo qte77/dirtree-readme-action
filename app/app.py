0cc79516687deac8e5308a4451c9652637e93b5f
qte77 <93844790+qte77@users.noreply.github.com>
2024-07-01 15:28:24 +0200

    Added default env vars

--- app/app.py
+++ app/app.py
@@ -3,21 +3,24 @@ from pathlib import Path
 from utils import get_tree_output, write_to_file
 
 
-CMD_HIGHLIGHT = str(getenv("CMD_HIGHLIGHT"))
+CMD_HIGHLIGHT = str(getenv("CMD_HIGHLIGHT", 'sh'))
 # EXCLUDE string separated by |
-EXCLUDE = str(getenv("EXCLUDE"))
+EXCLUDE = str(getenv("EXCLUDE", '.git|__pycache__'))
 INSERT_HERE_START_STRING = str(getenv(
   "INSERT_HERE_START_STRING"
+  '<!-- DIRTREE-README-ACTION-INSERT-HERE-START -->'
 ))
 INSERT_HERE_END_STRING = str(getenv(
-  "INSERT_HERE_END_STRING"
+  "INSERT_HERE_END_STRING",
+  '<!-- DIRTREE-README-ACTION-INSERT-HERE-END -->'
 ))
 OUT_FILE = str(getenv("OUT_FILE", 'README.md'))
-TREE_THEME = str(getenv("TREE_THEME"))
+TREE_THEME = str(getenv("TREE_THEME", 'sh'))
 
 
-startpath = Path('.')
+exclude_list = EXCLUDE.split('|')
 outfpath = Path(OUT_FILE)
+startpath = Path('.')
 
 
 assert startpath.exists(), f"{startpath} not found. Aborting"
@@ -27,7 +30,7 @@ if not outfpath.parent.exists():
 
 
 dirtree = get_tree_output(
-  startpath, EXCLUDE, CMD_HIGHLIGHT
+  startpath, exclude_list, CMD_HIGHLIGHT, TREE_THEME
 )
 write_to_file(
   outfpath, dirtree,
