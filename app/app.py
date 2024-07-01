d2a439955fd17242e45257296fa5e4cd361a73d4
qte77 <93844790+qte77@users.noreply.github.com>
2024-07-01 02:14:29 +0200

    Added f.truncate()

--- app/app.py
+++ app/app.py
@@ -25,7 +25,7 @@ if not outfpath.parent.exists():
 # try except
 dirtree = get_tree_output(startpath, exclude_list, CMD_HIGHLIGHT)
 
-with open(outfpath, 'w+') as f:
+with open(outfpath, 'r+') as f:
   # TODO remove redundant line loop if possible
   # will replace content between indices of first consecutive START and END
   # will only insert between START and END
@@ -48,3 +48,4 @@ with open(outfpath, 'w+') as f:
         for o in dirtree:
           f.write(o)
         printed = True
+    f.truncate()
