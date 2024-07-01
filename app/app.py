2538360b6304860ec51b24cfc12c1c0568832a56
qte77 <93844790+qte77@users.noreply.github.com>
2024-07-01 02:46:01 +0200

    Changed to separated file contexts for r and w

--- app/app.py
+++ app/app.py
@@ -15,6 +15,7 @@ INSERT_HERE_END_STRING = str(getenv(
 startpath = Path('.')
 outfpath = Path(OUT_FILE)
 exclude_list = EXCLUDE.split('|')
+sdx, edx, printed = None, None, False
 
 assert startpath.exists(), f"{startpath} not found. Aborting"
 
@@ -25,29 +26,28 @@ if not outfpath.parent.exists():
 # try except
 dirtree = get_tree_output(startpath, exclude_list, CMD_HIGHLIGHT)
 
-with open(outfpath, 'r+') as f:
+with open(outfpath, 'r') as f:
   # TODO remove redundant line loop if possible
   # will replace content between indices of first consecutive START and END
   # will only insert between START and END
   # will not insert if no match of START and END
-  sdx, edx, printed = None, None, False
   for index, line in enumerate(f):
     if line.startswith(INSERT_HERE_START_STRING):
       sdx = index
     elif line.startswith(INSERT_HERE_END_STRING) and sdx:
       edx = index
       break
-  # f.seek(0)
-  if sdx and edx:
-    print(f"{sdx=}, {edx=}")
-    for index, line in enumerate(f):
+
+if sdx and edx:
+  with open(outfpath, 'w') as f:
+      print(f"{sdx=}, {edx=}")
+      for index, line in enumerate(f):
+        print(f"{index=}, {line=}")
+        if index <= sdx or index >= edx:
+          f.write(line)
+        elif not printed:
+          print(f"{printed=}")
+          for o in dirtree:
+            f.write(o)
+          printed = True
       print(f"{index=}, {line=}")
-      if index <= sdx or index >= edx:
-        f.write(line)
-      elif not printed:
-        print(f"{printed=}")
-        for o in dirtree:
-          f.write(o)
-        printed = True
-    print(f"{index=}, {line=}")
-    # f.truncate()
