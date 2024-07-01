b735f80dc16ff5431ecea2c531812285b6c01732
qte77 <93844790+qte77@users.noreply.github.com>
2024-07-01 02:53:54 +0200

    Changed to nested open

--- app/app.py
+++ app/app.py
@@ -26,28 +26,28 @@ if not outfpath.parent.exists():
 # try except
 dirtree = get_tree_output(startpath, exclude_list, CMD_HIGHLIGHT)
 
-with open(outfpath, 'r') as f:
-  # TODO remove redundant line loop if possible
-  # will replace content between indices of first consecutive START and END
-  # will only insert between START and END
-  # will not insert if no match of START and END
-  for index, line in enumerate(f):
-    if line.startswith(INSERT_HERE_START_STRING):
-      sdx = index
-    elif line.startswith(INSERT_HERE_END_STRING) and sdx:
-      edx = index
-      break
-
-if sdx and edx:
-  with open(outfpath, 'w') as f:
-      print(f"{sdx=}, {edx=}")
-      for index, line in enumerate(f):
-        print(f"{index=}, {line=}")
-        if index <= sdx or index >= edx:
-          f.write(line)
-        elif not printed:
-          print(f"{printed=}")
-          for o in dirtree:
-            f.write(o)
-          printed = True
+# TODO read and write while avoiding copying to memory
+with open(outfpath, 'r') as f_in:
+  with open(outfpath, 'w') as f_out:
+    # TODO remove redundant line loop if possible
+    # will replace content between indices of first consecutive START and END
+    # will only insert between START and END
+    # will not insert if no match of START and END
+    for index, line in enumerate(f_in):
+      if line.startswith(INSERT_HERE_START_STRING):
+        sdx = index
+      elif line.startswith(INSERT_HERE_END_STRING) and sdx:
+        edx = index
+        break
+    # if sdx and edx:
+    print(f"{sdx=}, {edx=}")
+    for index, line in enumerate(f_in):
       print(f"{index=}, {line=}")
+      if index <= sdx or index >= edx:
+        f_out.write(line)
+      elif not printed:
+        print(f"{printed=}")
+        for o in dirtree:
+          f_out.write(o)
+        printed = True
+    print(f"{index=}, {line=}")
