73187c61739455a52e759c3ae646e11d783a1eb5
qte77 <qte@77>
2024-12-31 20:35:09 +0100

    refactor(app,action): Added docstrings, Changed Dockerfile, Renamed .yml to .yaml

--- app/app.py
+++ app/app.py
@@ -1,49 +1,146 @@
+"""
+This module contains the main script for the dirtree-readme-action, which generates
+and writes a directory tree to a specified file, typically README.md. It uses GitHub
+API to push changes back to the repository if not running from a local action.
+
+Key functionalities:
+- Reads environment variables for configuration.
+- Generates a formatted directory tree.
+- Writes the tree to a file between specified markers.
+- Optionally pushes changes to GitHub if not running locally.
+
+Environment Variables:
+- CMD_HIGHLIGHT: Syntax highlighting language for the tree output.
+- EXCLUDE: Directories or files to exclude from the tree.
+- INSERT_HERE_START_STRING: Start marker for tree insertion in the file.
+- INSERT_HERE_END_STRING: End marker for tree insertion in the file.
+- OUT_FILE: The file to write the directory tree to.
+- TREE_THEME: Theme for the tree structure.
+- USE_FROM_LOCAL_ACTION: Flag to determine if the action is running locally.
+- GH_TOKEN: GitHub token for authentication.
+- REPOSITORY: GitHub repository name.
+- COMMITTER_NAME: Name of the committer for the commit.
+- COMMITTER_EMAIL: Email of the committer for the commit.
+
+Functions:
+- push_file_to_github(files: str) -> None: Pushes the updated file to GitHub.
+
+Main Execution:
+- Checks for the existence of the output file and start path.
+- Generates and writes the directory tree to the specified file.
+- Pushes changes to GitHub if not running locally.
+
+Error Handling:
+- Uses logging to capture and report errors during execution.
+"""
+from logging import (
+    basicConfig,
+    getLogger,
+    INFO,
+    error,
+    exception
+)
+from github import (
+    ContentFile,
+    Github,
+    GithubException,
+    InputGitAuthor,
+    Repository
+)
 from os import getenv
 from pathlib import Path
+from typing import Optional, Union
 from utils import (
-  get_formatted_tree_output,
-  get_write_positions_in_file,
-  write_to_file
+    get_formatted_tree_output,
+    get_write_positions_in_file,
+    write_to_file
 )
 
+# Configure logging
+basicConfig(level=INFO)
+logger = getLogger(__name__)
 
-CMD_HIGHLIGHT = str(getenv("CMD_HIGHLIGHT", 'sh'))
-# EXCLUDE string separated by |
-EXCLUDE = str(getenv("EXCLUDE", '.git|__pycache__'))
-INSERT_HERE_START_STRING = str(getenv(
-  "INSERT_HERE_START_STRING",
-  '<!-- DIRTREE-README-ACTION-INSERT-HERE-START -->'
+# Environment variables
+CMD_HIGHLIGHT: str = str(getenv("CMD_HIGHLIGHT", 'sh'))
+EXCLUDE: str = str(getenv("EXCLUDE", '.git|__pycache__'))
+INSERT_HERE_START_STRING: str = str(getenv(
+    "INSERT_HERE_START_STRING",
+    '<!-- DIRTREE-README-ACTION-INSERT-HERE-START -->'
 ))
-INSERT_HERE_END_STRING = str(getenv(
-  "INSERT_HERE_END_STRING",
-  '<!-- DIRTREE-README-ACTION-INSERT-HERE-END -->'
+INSERT_HERE_END_STRING: str = str(getenv(
+    "INSERT_HERE_END_STRING",
+    '<!-- DIRTREE-README-ACTION-INSERT-HERE-END -->'
 ))
-OUT_FILE = str(getenv("OUT_FILE", 'README.md'))
-TREE_THEME = str(getenv("TREE_THEME", 'sh'))
+OUT_FILE: str = str(getenv("OUT_FILE", 'README.md'))
+TREE_THEME: str = str(getenv("TREE_THEME", 'sh'))
+USE_FROM_LOCAL_ACTION: str = str(getenv("USE_FROM_LOCAL_ACTION", "NO"))
 
+GH_TOKEN: Optional[str] = str(getenv("INPUT_GH_TOKEN"))
+REPOSITORY: Optional[str] = str(getenv("INPUT_REPOSITORY"))
+COMMITTER_NAME: str = str(getenv("INPUT_COMMITTER_NAME", "NOT_SET"))
+COMMITTER_EMAIL: str = str(getenv("INPUT_COMMITTER_EMAIL", "NOT_SET"))
+COMMIT_MESSAGE: str = f"Added dirtree to {OUT_FILE}"
 
-exclude_list = EXCLUDE.split('|')
-outfpath = Path(OUT_FILE)
-startpath = Path('.')
+exclude_list: list[str] = EXCLUDE.split('|')
+outfpath: Path = Path(OUT_FILE)
+startpath: Path = Path('.')
 
+# TODO use built-in runner git instead of gh-cli
+# TODO commit, push, pr, tag, delete former if failure() 
+def push_file_to_github(files: Union[str, Path]) -> None:
+    """
+    Push the updated file to GitHub repository.
 
-assert outfpath.exists(), f"{outfpath} not found. Aborting"
-assert startpath.exists(), f"{startpath} not found. Aborting"
+    :param files: Path to the file to be pushed.
+    :raises GithubException: If there's an error with the GitHub API.
+    """
+    try:
+        gh_connect = Github(GH_TOKEN)
+        gh_repo: Repository = gh_connect.get_repo(REPOSITORY)
+        gh_tree = gh_repo.get_git_tree(sha)
 
+        if COMMITTER_NAME != "NOT_SET" and COMMITTER_EMAIL != "NOT_SET":
+            committer: InputGitAuthor = InputGitAuthor(name=COMMITTER_NAME, email=COMMITTER_EMAIL)
+        else:
+            committer = None
 
-start_index, end_index = get_write_positions_in_file(
-  outfpath, INSERT_HERE_START_STRING, INSERT_HERE_END_STRING
-)
-if isinstance(start_index, int) \
-  and isinstance(end_index, int) \
-  and start_index>= 0 and end_index >= 1:
-  dirtree = get_formatted_tree_output(
-    startpath, exclude_list, CMD_HIGHLIGHT, TREE_THEME
-  )
-  write_to_file(
-    outfpath, dirtree, start_index, end_index
-  )
-else:
-  print(
-    f"Could not write file. Index error: {start_index=}, {end_index=}"
-  )
+        gh_repo.create_git_commit(COMMIT_MESSAGE, gh_tree, [], committer=committer)
+        gh_repo.push()
+    except GithubException as e:
+        error(f"GitHub API error: {e}")
+
+if __name__ == "__main__":
+    """
+    Main execution block to generate and write directory tree to README.md.
+    
+    This block:
+    - Checks if the output file and start path exist.
+    - Generates the directory tree.
+    - Writes the tree to the specified file.
+    - Optionally pushes the updated file to GitHub.
+    """
+    try:
+        assert outfpath.exists(), f"{outfpath} not found. Aborting"
+        assert startpath.exists(), f"{startpath} not found. Aborting"
+
+        start_index, end_index = get_write_positions_in_file(
+            outfpath, INSERT_HERE_START_STRING, INSERT_HERE_END_STRING
+        )
+        if isinstance(start_index, int) \
+           and isinstance(end_index, int) \
+           and start_index >= 0 and end_index >= 1:
+            dirtree = get_formatted_tree_output(
+                startpath, exclude_list, CMD_HIGHLIGHT, TREE_THEME
+            )
+            write_to_file(
+                outfpath, dirtree, start_index, end_index
+            )
+            if USE_FROM_LOCAL_ACTION == "NO":
+                push_file_to_github(outfpath)
+        else:
+            error(
+            	f"Could not write file. Index error: {start_index=}, {end_index=}"
+            )
+    except Exception as e:
+        exception(f"An error occurred: {e}")
+
--- app/utils.py
+++ app/utils.py
@@ -1,132 +1,118 @@
 '''Contains utility functions for Github dirtree-readme-action'''
 
-
 from collections import deque
 from datetime import datetime, timezone
 from pathlib import Path
 from typing import Iterator, Tuple
 
-
 def _get_tree_theme(theme: str = 'sh') -> Tuple[str, str, str, str]:
-  '''
-  Returns tuple of tree indicator themes: space, branch, tee, last.
-  Offers 'cmd', 'slash', 'elli', 'null', 'sh'. Defaults to 'sh'.
-  '''
-  if theme == 'cmd':
-    return '   ', '│  ', '├──', '└──'
-  elif theme == 'slash':
-    return '    ', '│   ', '│── ', '\── '
-  elif theme == 'elli':
-    return '    ', '︙   ', '︙··· ', ' ···· '
-  elif theme == 'null':
-    return '    ', '    ', '    ', '    '
-  elif theme == 'sh': 
-    return '    ', '│   ', '├── ', '└── '
-  else:
-    raise NotImplementedError
+    if theme == 'cmd':
+        return ' ', '│ ', '├──', '└──'
+    elif theme == 'slash':
+        return ' ', '│ ', '│── ', '\── '
+    elif theme == 'elli':
+        return ' ', '︙ ', '︙··· ', ' ···· '
+    elif theme == 'null':
+        return ' ', ' ', ' ', ' '
+    elif theme == 'sh':
+        return ' ', '│ ', '├── ', '└── '
+    else:
+        raise NotImplementedError
 
-
-def _is_path_in_exclude(
-  path: Path, exclude_list: list
-) -> bool:
-  '''Return True if any of exclude_list in path, else False'''
-  assert isinstance(path, Path)
-  assert isinstance(exclude_list, list)
-  for pt in path.parts:
-    if pt in exclude_list:
-      return True
-  return False
-
+def _is_path_in_exclude(path: Path, exclude_list: list) -> bool:
+    '''Return True if any of exclude_list in path, else False'''
+    assert isinstance(path, Path)
+    assert isinstance(exclude_list, list)
+    for pt in path.parts:
+        if pt in exclude_list:
+            return True
+    return False
 
 # list-directory-tree-structure-in-python:
 # https://stackoverflow.com/a/59109706
 def _generate_tree(
-  path: Path, exclude_list: list,
-  space: str, branch: str, tee: str, last: str,
-  prefix: str = '', suffix: str = ''
+    path: Path, exclude_list: list,
+    space: str, branch: str, tee: str, last: str,
+    prefix: str = '', suffix: str = ''
 ) -> Iterator[str]:
-  '''
-  A recursive generator, given a directory Path object
-  will yield a visual tree structure line by line
-  with each line prefixed by the same characters.
-  Returns a string of the current folder or file
-  and hierarchical indicators.
-  '''
-  contents = list(path.iterdir())
-  # contents each get pointers that are 'tee' with a final 'last'
-  pointers = [tee] * (len(contents) - 1) + [last]
-  for pointer, path in zip(pointers, contents):
-    if not _is_path_in_exclude(path, exclude_list):
-      yield prefix + pointer + path.name + suffix
-      if path.is_dir():
-          extension = branch if pointer == tee else space
-          yield from _generate_tree(
-            path, exclude_list,
-            space, branch, tee, last,
-            prefix + extension, suffix
-          )
-
+    ''' 
+    A recursive generator, given a directory Path object 
+    will yield a visual tree structure line by line 
+    with each line prefixed by the same characters. 
+    Returns a string of the current folder or file 
+    and hierarchical indicators. 
+    '''
+    contents = list(path.iterdir())
+    # contents each get pointers that are 'tee' with a final 'last'
+    pointers = [tee] * (len(contents) - 1) + [last]
+    for pointer, path in zip(pointers, contents):
+        if not _is_path_in_exclude(path, exclude_list):
+            yield prefix + pointer + path.name + suffix
+        if path.is_dir():
+            extension = branch if pointer == tee else space
+            yield from _generate_tree(
+                path, exclude_list,
+                space, branch, tee, last,
+                prefix + extension, suffix
+            )
 
 def get_formatted_tree_output(
-  startpath: Path, exclude_list: list,
-  cmd_highlight: str, tree_theme: str
+    startpath: Path, exclude_list: list,
+    cmd_highlight: str, tree_theme: str
 ) -> deque[str]:
-  '''
-  Returns a list of startpath and its children. cmd_highlight has
-  to be one of Github's native syntax highlighting languages.
-  https://github.com/github-linguist/linguist/blob/master/lib/linguist/languages.yml
-  '''
-  suffix = '\n'
-  space, branch, tee, last = _get_tree_theme(tree_theme)
-  dirtree = _generate_tree(
-    startpath, exclude_list,
-    space, branch, tee, last,
-    suffix = suffix
-  )
-  out = deque(dirtree)
-  out.appendleft(f"{datetime.now(timezone.utc)}{suffix}")
-  out.appendleft(f"```{cmd_highlight}{suffix}")
-  out.append(f"```{suffix}")  
-  return out
-
+    ''' 
+    Returns a list of startpath and its children. cmd_highlight has 
+    to be one of Github's native syntax highlighting languages. 
+    https://github.com/github-linguist/linguist/blob/master/lib/linguist/languages.yml 
+    '''
+    suffix = '\n'
+    space, branch, tee, last = _get_tree_theme(tree_theme)
+    dirtree = _generate_tree(
+        startpath, exclude_list,
+        space, branch, tee, last,
+        suffix = suffix
+    )
+    out = deque(dirtree)
+    out.appendleft(f"{datetime.now(timezone.utc)}{suffix}")
+    out.appendleft(f"```
+    out.append(f"```{suffix}")
+    return out
 
 def get_write_positions_in_file(
-  outfpath: Path, start_string: str, end_string: str
+    outfpath: Path, start_string: str, end_string: str
 ) -> Tuple[int, int]:
-  '''
-  Returns position of first consecutive start_string
-  and end_string.
-  '''
-  sdx, edx = None, None
-  f_in = (line for line in open(outfpath, 'r'))
-  for index, line in enumerate(f_in):
-    if line.startswith(start_string):
-      sdx = index
-    elif line.startswith(end_string) and sdx:
-      edx = index
-      break
-  return sdx, edx
-
+    ''' 
+    Returns position of first consecutive start_string 
+    and end_string. 
+    '''
+    sdx, edx = None, None
+    f_in = (line for line in open(outfpath, 'r'))
+    for index, line in enumerate(f_in):
+        if line.startswith(start_string):
+            sdx = index
+        elif line.startswith(end_string) and sdx:
+            edx = index
+            break
+    return sdx, edx
 
 def write_to_file(
-  outfpath: Path, dirtree: deque,
-  start_index: int, end_index: int
+    outfpath: Path, dirtree: deque,
+    start_index: int, end_index: int
 ) -> None:
-  '''
-  Replaces content between indices start_index and end_index.
-  At least one line between start_index and end end_index needed.
-  '''
-  outfpath_temp = outfpath.with_suffix(".temp_outfile_ghact")
-  assert start_index >= 0 and end_index >= 1, \
-    f"Can not insert: {start_index=}, {end_index=}"
-  f_in = (line for line in open(outfpath, 'r'))
-  # TODO write lines with generator
-  # f_out = (line for line in open(outfpath, 'w'))
-  with open(outfpath_temp, 'w') as f_out:
-    for index, line in enumerate(f_in):
-      if index <= start_index or index >= end_index:
-        f_out.write(line)
-      elif index == start_index + 1:
-        f_out.writelines(dirtree)
-  outfpath.unlink() # missing_ok=True
-  outfpath_temp.rename(outfpath)
+    ''' 
+    Replaces content between indices start_index and end_index. 
+    At least one line between start_index and end end_index needed. 
+    '''
+    outfpath_temp = outfpath.with_suffix(".temp_outfile_ghact")
+    assert start_index >= 0 and end_index >= 1, \
+        f"Can not insert: {start_index=}, {end_index=}"
+    f_in = (line for line in open(outfpath, 'r'))
+    with open(outfpath_temp, 'w') as f_out:
+        for index, line in enumerate(f_in):
+            if index <= start_index or index >= end_index:
+                f_out.write(line)
+            elif index == start_index + 1:
+                f_out.writelines(dirtree)
+    outfpath.unlink() # missing_ok=True
+    outfpath_temp.rename(outfpath)
+
